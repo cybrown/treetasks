d54cc60942377f8054172fd57ebb156f927a64ba
Cy Brown <cy@cybrown.fr>
2014-08-10 20:52:21 +0200

    Refactoring

--- public/js/application.js
+++ public/js/application.js
@@ -5,26 +5,23 @@ var TaskBaseController = function (taskService) {
     this.refreshData();
 };
 
-TaskBaseController.prototype.setDone = function (taskId) {
-    this.taskService.setDone(taskId);
-    this.refreshData();
-};
-
-TaskBaseController.prototype.setUndone = function (taskId) {
-    this.taskService.setUndone(taskId);
-    this.refreshData();
-};
-
-TaskBaseController.prototype.create = function (taskId, description) {
+TaskBaseController.prototype.save = function (task) {
     var _this = this;
-    this.taskService.create(taskId, description).then(function () {
+    this.taskService.save(task).then(function () {
         _this.refreshData();
     });
 };
 
-TaskBaseController.prototype.delete = function (taskId) {
+TaskBaseController.prototype.create = function (task) {
     var _this = this;
-    this.taskService.delete(taskId).then(function () {
+    this.taskService.create(task).then(function () {
+        _this.refreshData();
+    });
+};
+
+TaskBaseController.prototype.delete = function (task) {
+    var _this = this;
+    this.taskService.delete(task.id).then(function () {
         _this.refreshData();
     });
 };
@@ -100,6 +97,24 @@ TaskDetailsController.prototype.findTasks = function () {
 };
 // </editor-fold>
 
+// <editor-fold description="TaskSearchController">
+var TaskSearchController = function (taskService) {
+    this.taskService = taskService;
+    this.tasks = [];
+};
+
+TaskSearchController.prototype.search = function () {
+    var _this = this;
+    this.taskService.findBySearch(this.searchTerm).then(function (tasks) {
+        _this.tasks = tasks;
+    });
+};
+
+TaskSearchController.prototype.findTasks = function () {
+    return this.tasks;
+};
+// </editor-fold>
+
 // <editor-fold description="TaskService">
 var TaskService = function ($http, BASE_URL) {
     this.$http = $http;
@@ -130,126 +145,193 @@ TaskService.prototype.findById = function (taskId) {
     });
 };
 
-TaskService.prototype.setDone = function (taskId) {
-    return this.$http.post(this.BASE_URL + taskId + '/done').then(function (response) {
-        console.log(response);
-    })
-};
-
-TaskService.prototype.setUndone = function (taskId) {
-    return this.$http.post(this.BASE_URL + taskId + '/undone').then(function (response) {
-        console.log(response);
-    })
-};
-
-TaskService.prototype.create = function (parentId, description) {
-    return this.$http.put(this.BASE_URL, {
-        parentId: parentId,
-        description: description
+TaskService.prototype.findBySearch = function (searchTerm) {
+    return this.$http.get(this.BASE_URL + 'search', {
+        params: {
+            q: searchTerm
+        }
     }).then(function (response) {
         return response.data;
     });
 };
 
+TaskService.prototype.save = function (task) {
+    return this.$http.post(this.BASE_URL + task.id, task);
+};
+
+TaskService.prototype.create = function (task) {
+    return this.$http.put(this.BASE_URL, task);
+};
+
 TaskService.prototype.delete = function (taskId) {
-    return this.$http.delete(this.BASE_URL + taskId).then(function (response) {
-        return response.data;
-    });
+    return this.$http.delete(this.BASE_URL + taskId);
 };
 // </editor-fold>
 
-// <editor-fold description="TaskSearchController">
-var TaskSearchController = function (taskService) {
-    this.taskService = taskService;
+// <editor-fold description="ClipService">
+var ClipService = function () {
+    this.data = null;
+    this.type = null;
 };
 
-TaskSearchController.prototype.findAll = function () {
-    return this.taskService.findAll();
+ClipService.prototype.hasData = function (type) {
+    return this.type === type;
 };
 
-TaskSearchController.prototype.search = function () {
-    this.taskService.search(this.searchTerm).then(function () {
-
-    })
+ClipService.prototype.setData = function (data, type) {
+    this.type = type;
+    this.data = data;
 };
+
+ClipService.prototype.getData = function () {
+    return this.data;
+};
+
+ClipService.prototype.clear = function () {
+    this.data = null;
+    this.type = null;
+};
+// </editor-fold>
+
+// <editor-fold description="TaskCreateDirective module">
+var TaskCreateDirective = function (taskService) {
+    return {
+        restrict: 'E',
+        scope: {
+            onCreate: '&'
+        },
+        templateUrl: 'views/directives/task-create.html',
+        link: function (scope, elem, attrs) {
+            scope.create = function (task) {
+                scope.onCreate({task: task});
+            };
+        }
+    }
+};
+// </editor-fold>
+
+// <editor-fold description="TaskListDirective module">
+var TaskListDirective = function (taskService, clipService) {
+    return {
+        restrict: 'E',
+        scope: {
+            tasks: '=',
+            onCreate: '&',
+            onSave: '&',
+            onDelete: '&'
+        },
+        templateUrl: 'views/directives/task-list.html',
+        link: function (scope, elem, attrs) {
+            scope.cut = function (task) {
+                clipService.setData(task, 'task');
+            };
+            scope.paste = function (parentTask) {
+                if (clipService.hasData('task')) {
+                    var task = clipService.getData();
+                    task.parentId = parentTask.id;
+                    clipService.clear();
+                    scope.onSave({task: task});
+                }
+            };
+            scope.showReleaseBtn = function (task) {
+                if (clipService.hasData('task')) {
+                    return clipService.getData().id === task.id;
+                }
+                return false;
+            };
+            scope.showPasteBtn = function (task) {
+                if (clipService.hasData('task')) {
+                    return clipService.getData().id !== task.id;
+                }
+                return false;
+            };
+            scope.showCutBtn = function (task) {
+                return !clipService.hasData('task');
+            };
+            scope.setDone = function (task, done) {
+                task.done = done;
+                scope.onSave({task: task});
+            };
+            scope.create = function (task) {
+                scope.onCreate({task: task});
+            };
+            scope.remove = function (task) {
+                scope.onDelete({task: task});
+            };
+            scope.release = function (task) {
+                task.parentId = 0;
+                clipService.clear();
+                scope.onSave({task: task});
+            };
+        }
+    }
+};
+// </editor-fold>
+
+// <editor-fold description="ROUTES constant">
+var ROUTES = [{
+    name: 'root',
+    templateUrl: 'views/common/layout.html',
+    abstract: true
+}, {
+    name: 'tasks',
+    url: '',
+    views: {
+        main: {
+            templateUrl: 'views/common/uiview.html'
+        }
+    },
+    abstract: true,
+    parent: 'root'
+}, {
+    name: 'tasks.todo',
+    url: '/',
+    templateUrl: 'views/tasks/todo.html',
+    controller: 'TaskTodoController',
+    controllerAs: 'ctrl'
+}, {
+    name: 'tasks.all',
+    url: '/all',
+    templateUrl: 'views/tasks/all.html',
+    controller: 'TaskAllController',
+    controllerAs: 'ctrl'
+}, {
+    name: 'tasks.details',
+    url: '/details/:id',
+    templateUrl: 'views/tasks/details.html',
+    controller: 'TaskDetailsController',
+    controllerAs: 'ctrl'
+}, {
+    name: 'tasks.search',
+    url: '/search',
+    templateUrl: 'views/tasks/search.html',
+    controller: 'TaskSearchController',
+    controllerAs: 'ctrl'
+}];
 // </editor-fold>
 
 // <editor-fold description="treeTaskApp module">
-angular.module('treeTaskApp', ['ui.router', 'cy'])
-    .config(function ($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise('/');
-        $stateProvider
-            .state('root', {
-                templateUrl: 'views/common/layout.html',
-                abstract: true
-            })
-            .state('tasks', {
-                url: '',
-                views: {
-                    main: {
-                        template: '<ui-view></ui-view>'
-                    }
-                },
-                abstract: true,
-                parent: 'root'
-            })
-            .state('tasks.todo', {
-                url: '/',
-                templateUrl: 'views/tasks/todo.html',
-                controller: TaskTodoController,
-                controllerAs: 'ctrl'
-            })
-            .state('tasks.all', {
-                url: '/all',
-                templateUrl: 'views/tasks/all.html',
-                controller: TaskAllController,
-                controllerAs: 'ctrl'
-            })
-            .state('tasks.details', {
-                url: '/details/:id',
-                templateUrl: 'views/tasks/details.html',
-                controller: TaskDetailsController,
-                controllerAs: 'ctrl'
-            })
-            .state('tasks.search', {
-                url: '/search',
-                templateUrl: 'views/tasks/search.html',
-                controller: TaskSearchController,
-                controllerAs: 'ctrl'
-            });
-    })
-    .run(function ($rootScope) {
-        $rootScope.$on('$stateChangeStart', function (event, toState, fromState, toParams, fromParams) {
-            console.log('sStateChangeStart: ' + fromState.name + ' to ' + toState.name);
-        });
-    })
+angular.module('treeTaskApp', ['ui.router', 'cy.util'])
+    .controller('TaskBaseController', TaskBaseController)
+    .controller('TaskAllController', TaskAllController)
+    .controller('TaskTodoController', TaskTodoController)
+    .controller('TaskDetailsController', TaskDetailsController)
+    .controller('TaskSearchController', TaskSearchController)
+    .constant('ROUTES', ROUTES)
     .service('taskService', TaskService)
-    .constant('BASE_URL', '/api/')
-    .directive('taskList', function (taskService) {
-        return {
-            restrict: 'E',
-            scope: {
-                showNew: '@',
-                tasks: '=',
-                create: '&',
-                delete: '&',
-                setDone: '&',
-                setUndone: '&',
-                currentTaskId: '@'
-            },
-            templateUrl: 'views/directives/task-list.html',
-            link: function (scope, elem, attrs) {
-                scope.showNew = JSON.parse(scope.showNew);
-                if (scope.currentTaskId) {
-                    scope.currentTaskId = Number(scope.currentTaskId);
-                }
-            }
-        }
+    .constant('BASE_URL', '/api/tasks/')
+    .directive('taskList', TaskListDirective)
+    .directive('taskCreate', TaskCreateDirective)
+    .config(function ($stateProvider, $urlRouterProvider, ROUTES) {
+        $urlRouterProvider.otherwise('/');
+        ROUTES.forEach(function (route) {
+            $stateProvider.state(route.name, route);
+        });
     });
 // </editor-fold>
 
-// <editor-fold description="cy module">
-angular.module('cy', [])
+// <editor-fold description="cy.util module">
+angular.module('cy.util', [])
     .directive('cyActiveOnState', function ($rootScope, $state) {
         return {
             restrict: 'A',
@@ -265,5 +347,6 @@ angular.module('cy', [])
                 $rootScope.$on('$stateChangeSuccess', changeState);
             }
         };
-    });
+    })
+    .service('clipService', ClipService);
 // </editor-fold>
