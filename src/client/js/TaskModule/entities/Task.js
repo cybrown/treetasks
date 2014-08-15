dd763774c799c581b971c16ffa06c77acf83f6fa
Cy Brown <cy@cybrown.fr>
2014-08-15 22:33:38 +0200

    moved task done logic in Task model

--- src/client/js/TaskModule/entities/Task.js
+++ src/client/js/TaskModule/entities/Task.js
@@ -5,8 +5,29 @@ var Task = module.exports = function () {
     this._syncStatus = 0;
 
     this.description = '';
-    this.done = false;
+    this._done = false;
     this.favorite = false;
     this.postrequisites = new ArraySet();
     this.prerequisites = new ArraySet();
 };
+
+Object.defineProperty(Task.prototype, 'done', {
+    get: function () {
+        return this._done;
+    },
+    set: function (value) {
+        var ok = true;
+        if (value) {
+            this.prerequisites.forEach(function (task) {
+                ok = ok && task.done;
+            });
+        } else {
+            this.postrequisites.forEach(function (task) {
+                ok = ok && !task.done;
+            });
+        }
+        if (ok) {
+            this._done = value;
+        }
+    }
+});
